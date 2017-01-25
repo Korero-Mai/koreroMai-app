1f5def5f19325dd22cba82252d36a3540faab7c4
mischa-saunders <mischa.saunders@gmail.com>
2017-01-25 16:53:17 +1300

    rendering react!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const React = require('react')
-const ReactDOM = require('ReactDOM')
+const ReactDOM = require('react-dom')
+const { Provider } = require('react-redux')
+const { createStore } = require('redux')
+const createHistory = require('history').createHashHistory
+const { Router, Route, IndexRoute, hashHistory } = require('react-router')
+
+const reducer = require('./reducer')
+const initialState = require('../state')
+
+//Top Level Components
+const App = require('./components/app')
+
+const store = createStore(reducer, initialState)
+
 const Root = ({store}) => {
 	return (
 			<Provider store = {store}>
@@ -9,3 +22,11 @@ const Root = ({store}) => {
 			</Provider>
 	)
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+	const root = document.querySelector('#app')
+		ReactDOM.render(
+			<Root store={store}/>,
+			root
+		)
+})
