4a707b64778b0e4b70a7bed06879153c1cbf5768
mischa-saunders <mischa.saunders@gmail.com>
2017-01-26 10:58:42 +1300

    basic layout with table and radiobuttons

--- src/components/containers/nightOutPage.js
+++ src/components/containers/nightOutPage.js
@@ -6,6 +6,52 @@ const NightOut = (props) => {
 	return (
 		<div>
 			<h1>Night Out </h1>
+
+			<div>
+				TOTAL FOR NIGHT
+			</div><br/>
+			<form>
+				<table>
+
+					<thead>
+						<tr>
+							<th>
+								Names
+							</th>
+							<th>
+								Person picking up tab
+							</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						<tr>
+							<td>
+								PersonA
+							</td>
+							<td>
+
+								<input type="radio" name="payer" />
+
+							</td>
+						</tr>
+						<tr>
+							<td>
+								PersonB
+							</td>
+							<td>
+
+								<input type="radio" name="payer" />
+
+							</td>
+						</tr>
+					</tbody>
+
+				</table>
+			</form><br/>
+			<div>
+				ADD ANOTHER PERSON
+			</div>
 		</div>
 
 	)
