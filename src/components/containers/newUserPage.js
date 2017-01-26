c49e42e0e3b3cb527689ab7b6fa83649dbcc1f8f
anna-marie-vos <amvos@outlook.com>
2017-01-26 15:47:47 +1300

    grabs data from the form but doesn't go anywhere yet

--- src/components/containers/newUserPage.js
+++ src/components/containers/newUserPage.js
@@ -5,22 +5,55 @@ const { Link } = require('react-router')
 const ReactDOM = require('react-dom')
 const {TextField}= require('material-ui');
 const {RaisedButton} = require('material-ui')
+const request = require('superagent')
 
-const NewUser = (props) => {
+const NewUser = React.createClass({
+	handleSubmit:function(e){
+		e.preventDefault()
+	console.log("this.refs.userName",this.refs.userName.value);
+	const userName = this.refs.userName.value
+	const email = this.refs.email.value
+	const password = this.refs.password.value
+	const confirmPassword = this.refs.confirmPassword.value
+
+	const newUserData = {
+		'userName':userName,
+		'email':email,
+		'password':password
+	}
+
+	if(userName.length>0){
+		request.post('api/v1/newUserPage')
+		.send({newUserData})
+		.end((err, data)=>{
+			console.log('post from newUserPage newUserData:',newUserData);
+			this.props.store.dispatch({type: 'POST_NEW_USER', payload: data.body})
+		})
+		this.refs.userName.input.value = ''
+		this.refs.email.input.value = ''
+		this.refs.password.input.value = ''
+		this.refs.confirmPassword.input.value =''
+
+	} else{
+		this.refs.userName.focus()
+	}
+},
+render: function(){
 
 	return (
 		<div>
 		<h2>New User Form</h2>
 		<form>
-			<p>Name:</p><input type='text' ref='addNewUser' placeholder='User Name' />
-			<p>Email:</p><input type='text' ref='addNewUser' placeholder='email' />
-		  <p>Password:</p><input type='text' ref='addNewUser' placeholder='password' />
-			<p>Confirm Password:</p><input type='text' ref='addNewUser' placeholder='Add a new user' />
-			<button className='button'>Submit</button>
+			<p>Name:</p><input type='text' ref='userName' placeholder='User Name' />
+			<p>Email:</p><input type='text' ref='email' placeholder='email' />
+		  <p>Password:</p><input type='text' ref='password' placeholder='password' />
+			<p>Confirm Password:</p><input type='text' ref='confirmPassword' placeholder='Confirm password' />
+			<button onClick={this.handleSubmit} className='button'>Creat Acount</button>
 		</form>
 		</div>
 	)
 }
+})
 
 module.exports = connect((state) => state)(NewUser)
 
