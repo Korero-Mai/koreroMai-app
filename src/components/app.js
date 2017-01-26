0378fd3851e472da1e93e8919b1902de53c26167
Janie Tito <jtito@hotmail.com>
2017-01-26 15:59:27 +1300

    add classNames to lists

--- src/components/app.js
+++ src/components/app.js
@@ -14,10 +14,10 @@ const App = (props) => {
 			<img src="http://www.kofusion.com/wp-content/uploads/2009/02/featured_drinks.jpg" width='100px'/>
 			<h1>Drink Splitter</h1>
 			<nav>
-				<ul>
-					<li><Link to="/">Home</Link></li>
-					<li><Link to="/userprofile">Your Profile</Link></li>
-					<li><Link to="/nightout">Current Night Out</Link></li>
+				<ul className="navList">
+					<li className="navListItem"><Link to="/">Home</Link></li>
+					<li className="navListItem"><Link to="/userprofile">Your Profile</Link></li>
+					<li className="navListItem"><Link to="/nightout">Current Night Out</Link></li>
 				</ul>
 			</nav>
 
