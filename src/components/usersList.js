e7bf2152bdc020727ec86020876f68f047b4c280
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-01-26 18:13:26 +1300

    Make nice table

--- src/components/usersList.js
+++ src/components/usersList.js
@@ -10,53 +10,33 @@ const UsersList = (props) => {
 
   return (
 
-  <div>
-    <h1>All the Users</h1>
-    <ul>
-      {_.map(users, (user) => {
-        return <li key={user.id}>ID {user.id}: {user.name}</li>
-      })
-    }
-  </ul>
-  </div>
-
-)
-
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHeaderColumn>ID</TableHeaderColumn>
+          <TableHeaderColumn>Name</TableHeaderColumn>
+          <TableHeaderColumn>Latest night out</TableHeaderColumn>
+          <TableHeaderColumn>Number of nights out</TableHeaderColumn>
+          <TableHeaderColumn>Total owed</TableHeaderColumn>
+          <TableHeaderColumn>Total owing</TableHeaderColumn>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+          {_.map(users, (user) => {
+            return (
+              <TableRow>
+                <TableRowColumn>{user.id}</TableRowColumn>
+                <TableRowColumn>{user.name}</TableRowColumn>
+                <TableRowColumn>01/01/2017</TableRowColumn>
+                <TableRowColumn>10</TableRowColumn>
+                <TableRowColumn>100 $</TableRowColumn>
+                <TableRowColumn>0 $</TableRowColumn>
+              </TableRow>
+            )
+          })}
+      </TableBody>
+    </Table>
+  )
 }
 
 module.exports = connect((state) => state)(UsersList)
-
-
-//
-//     <Table>
-//   <TableHeader>
-//     <TableRow>
-//       <TableHeaderColumn>ID</TableHeaderColumn>
-//       <TableHeaderColumn>Name</TableHeaderColumn>
-//       <TableHeaderColumn>Status</TableHeaderColumn>
-//     </TableRow>
-//   </TableHeader>
-//   <TableBody>
-//     <TableRow>
-//       <TableRowColumn>1</TableRowColumn>
-//       <TableRowColumn>John Smith</TableRowColumn>
-//       <TableRowColumn>Employed</TableRowColumn>
-//     </TableRow>
-//     <TableRow>
-//       <TableRowColumn>2</TableRowColumn>
-//       <TableRowColumn>Randal White</TableRowColumn>
-//       <TableRowColumn>Unemployed</TableRowColumn>
-//     </TableRow>
-//     <TableRow>
-//       <TableRowColumn>3</TableRowColumn>
-//       <TableRowColumn>Stephanie Sanders</TableRowColumn>
-//       <TableRowColumn>Employed</TableRowColumn>
-//     </TableRow>
-//     <TableRow>
-//       <TableRowColumn>4</TableRowColumn>
-//       <TableRowColumn>Steve Brown</TableRowColumn>
-//       <TableRowColumn>Employed</TableRowColumn>
-//     </TableRow>
-//   </TableBody>
-// </Table>
-// 	)
