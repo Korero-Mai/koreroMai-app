a3565c107052eb495e63c8afa39c0e77943068ea
mischa-saunders <mischa.saunders@gmail.com>
2017-01-26 15:38:00 +1300

    add update users test

--- src/reducer.js
+++ src/reducer.js
@@ -1,21 +1,14 @@
 const clone = require('clone')
 
 module.exports = function (state, action){
-	return state
+	const newState=clone(state)
+	switch (action.type) {
+
+		case 'UPDATE_USERS':
+		newState.users = action.payload
+		return newState
+
+		default:
+		return state
+	}
 }
-
-
-//
-// function reducer(state,action) {
-//
-// 	const newState=clone(state)
-// 	switch (action.type) {
-//
-// 		case 'GET_ALL_USERS':
-// 		newState.users = action.payload
-// 		return newState
-//
-// 		default:
-// 		return state
-// 	}
-// }
