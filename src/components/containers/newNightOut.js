28df29913731bfd341ff979d1f7b55f11ff6b368
JanieTito <jtito@hotmail.com>
2017-01-27 16:04:21 +1300

    frak yeah

--- src/components/containers/newNightOut.js
+++ src/components/containers/newNightOut.js
@@ -5,18 +5,6 @@ const moment = require('moment')
 const _ = require('lodash')
 const { FlatButton } = require('material-ui')
 
-
-<<<<<<< HEAD
-const NewNightOut = (props) => {
-  const  { users, dispatch } = props
-
-  console.log("this is props", props);
-
-  const namesForDisplay = _.map(users, (user) => {
-    user.color = user.going
-      ?'pink'
-      :'white'
-
 class NewNightOut extends React.Component {
   render() {
     const  { users, dispatch, currentNight } = this.props
@@ -34,7 +22,7 @@ class NewNightOut extends React.Component {
         <h1>Add people to your group</h1>
         {
           namesForDisplay.map((name) => {
-            return <FlatButton style={{backgroundColor: name.color}} onClick={()=> dispatch({type:'ADDS_PERSON_TO_CURRENT_NIGHT', payload: name.id})}>
+            return <FlatButton style={{backgroundColor: name.color, margin:'10px', width: '20%'}} onClick={()=> dispatch({type:'ADDS_PERSON_TO_CURRENT_NIGHT', payload: name.id})}>
               {name.name}
             </FlatButton>
 
@@ -46,27 +34,5 @@ class NewNightOut extends React.Component {
       </div>
     )
   }
-
-<<<<<<< HEAD
-  return (
-    <div className='welcome'>
-      <p>{(moment().format('dddd DD MMMM YYYY'))}</p>
-      <h1>Add people to your group</h1>
-      <div >
-
-        {
-          namesForDisplay.map((name) => {
-            return <FlatButton style={{backgroundColor: name.color, margin:'10px', width: '20%'}} onClick={()=> dispatch({type:'ADDS_PERSON_TO_NEW_GROUP', payload: name.id})}>
-              {name.name}<br/><br/>
-            </FlatButton>
-
-          })
-        }<br/><br/>
-        <button>submit</button>
-      </div>
-    </div>
-  )
-=======
->>>>>>> 4f3aa1f23df88dc66b62e94972ce600a98afcae8
 }
 module.exports =  connect((state) => state)(NewNightOut)
