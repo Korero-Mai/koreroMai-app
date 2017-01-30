8e35074d085987467b0d1a4cca306c27cee47123
MeghanHughes <hughes.meghan@gmail.com>
2017-01-31 11:25:12 +1300

    routes are RESTful

--- api/resources.js
+++ api/resources.js
@@ -5,13 +5,12 @@ const bcrypt = require('bcryptjs')
 
 module.exports = function(db) {
 
-  // route.get('/users', getUsers); //gets all the users
-  route.get('/users/:id/profile') //display a specific user
-  route.get('/nights', getNights); //display all of the nights info
-  // route.get('/users_nights', getUsersNights);
-  route.get('/users/:id/nights', getUsersNights); //get a users nights
+  route.get('/users', users); //gets all the users
+  route.get('/users/:id/profile', userProfile) //display a specific user
+  route.get('/nights', nights); //display all of the nights info
+  route.get('/users/:id/nights', userNight); //get a users nights
   route.post('/', post);   //what does this do?
-  route.post('/users/new', postNewUser);   //post a new user
+  route.post('/users/new', userNew);   //post a new user
   route.post('/users/login', login) //login a user
 
   function login(req, res, next) {
@@ -33,28 +32,28 @@ module.exports = function(db) {
       })
   }
 
-  function getUsers(req, res, next) {
+  function users(req, res, next) {
     db.findAll('users')
       .then((users) => {
         res.json(users)
       })
   }
 
-  function getNights(req, res, next) {
+  function nights(req, res, next) {
     db.findAll('nights')
       .then((nights) => {
         res.json(nights)
       })
   }
 
-  function getUsersNights(req, res, next) {
+  function userNight(req, res, next) {
     db.findAll('users_nights')
       .then((users_nights) => {
         res.json(users_nights)
       })
   }
 
-  function postNewUser(req, res, next){
+  function userNew(req, res, next){
     const password = req.body.newUserData.password
     bcrypt.genSalt(10, function (err, salt) {
       bcrypt.hash(password, salt, function(err, hash) {
