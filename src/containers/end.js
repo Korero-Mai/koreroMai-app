016dded391ecd5d713368b48992a6cd15ecc1c2a
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-02-08 10:45:57 +1300

    Add scoring

--- src/containers/end.js
+++ src/containers/end.js
@@ -2,10 +2,11 @@ const React = require('react')
 const { connect } = require('react-redux')
 
 function End(props) {
+  const score = props.score
   return (
     <div>
       <h1>The End</h1>
-      <p>Your score is : [score be here]!</p>
+      <p>Your score is : {score} !</p>
     </div>
   )
 }
