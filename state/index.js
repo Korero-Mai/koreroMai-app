0c4e5a2b1980374a4681ff42bfbe2526675f56fc
anna-marie-vos <amvos@outlook.com>
2017-02-10 14:07:57 +1300

    adding letters to state

--- state/index.js
+++ state/index.js
@@ -1,5 +1,22 @@
 import { routerMiddleware, push } from 'react-router-redux'
 
 module.exports = {
-
+ letters: {
+   hi:{
+     soundFile: 'mp3',
+     imageFile: 'img',
+   },
+   te:{
+     soundFile: 'mp3',
+     imageFile: 'img',
+   },
+   ka:{
+     soundFile: 'mp3',
+     imageFile: 'img',
+   },
+   ph:{
+     soundFile: 'mp3',
+     imageFile: 'img',
+   }
+ }
 }
