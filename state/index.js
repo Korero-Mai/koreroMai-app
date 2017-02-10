993bb4542f4fc0ab807c8bba17214b440fa70bfd
anna-marie-vos <amvos@outlook.com>
2017-02-10 14:19:46 +1300

    soundpage added to state

--- state/index.js
+++ state/index.js
@@ -14,9 +14,24 @@ module.exports = {
      soundFile: 'mp3',
      imageFile: 'img',
    },
+   me:{
+     soundFile: 'mp3',
+     imageFile: 'img',
+   },
+   he:{
+     soundFile: 'mp3',
+     imageFile: 'img',
+   },
    ph:{
      soundFile: 'mp3',
      imageFile: 'img',
    }
- }
+ },
+ soundPage :{
+     1:['hi','te','ka'],
+     2:['me','he','ph'],
+     3:['ka','hi','te'],
+     4:['ph','me','te'],
+     5:['hi','me','ph']
+   }
 }
