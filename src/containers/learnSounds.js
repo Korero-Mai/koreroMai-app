fc765a30e65066cf631361328c4e7c3b4706c45e
MeghanHughes <hughes.meghan@gmail.com>
2017-02-11 08:31:16 +1300

    react class component setup

--- src/containers/learnSounds.js
+++ src/containers/learnSounds.js
@@ -1,43 +1,54 @@
 const React = require('react')
 const { connect } = require('react-redux')
 
-function LearnSounds(props) {
-  const lettersArr = props.learnSoundPage[props.params.id]
-  console.log('lettersArr', lettersArr);
-  const lettersObj = props.letters
-  return (
-      <div className="playBox">
-        {generateLetter(lettersArr, lettersObj, props)}
-        <div>
-          <button className="button radius">Back</button>
-          <button className="button radius">next</button>
+class LearnSounds extends React.Component {
+
+  handleClick() {
+
+    const { dispatch } = this.props
+
+    // const email = this.refs.email.value
+    const lettersArr = props.learnSoundPage[props.params.id]
+    const lettersObj = props.letters
+
+    function playSound() {
+      const sound = this.refs.gong.value
+      console.log('sound', sound);
+    }
+
+    function generateLetter(lettersArr,lettersObj, props){
+      console.log('learnSounds props',props)
+      return lettersArr.map((letter,index)=>{
+        return (
+          <div>
+          <audio ref="gong">
+          <source src={`${lettersObj[letter].soundFile}`} type="audio/mpeg" />
+          </audio>
+
+          <img src={`${lettersObj[letter].imageFile}`}/>
+          <button onClick={this.playSound.bind(this)} className={`button radius letter${index+1}Position`}>
+          {letter}
+          </button>
+
+          </div>
+        )
+      })
+    }
+  }
+
+  render() {
+    const { dispatch } = this.props
+
+    return (
+        <div className="playBox">
+          {generateLetter(lettersArr, lettersObj, props)}
+          <div>
+            <button className="button radius">Back</button>
+            <button className="button radius">next</button>
+          </div>
         </div>
-      </div>
     )
-}
+
+  }
 
 module.exports = connect((state) => state)(LearnSounds)
-
-function playSound() {
-  const sound = this.refs.gong.value
-  console.log('sound', sound);
-}
-
-function generateLetter(lettersArr,lettersObj, props){
-  console.log('learnSounds props',props)
-  return lettersArr.map((letter,index)=>{
-    return (
-      <div>
-      <audio ref="gong">
-      <source src={`${lettersObj[letter].soundFile}`} type="audio/mpeg" />
-      </audio>
-
-      <img src={`${lettersObj[letter].imageFile}`}/>
-      <button onClick={this.playSound.bind(this)} className={`button radius letter${index+1}Position`}>
-      {letter}
-      </button>
-
-      </div>
-    )
-  })
-}
