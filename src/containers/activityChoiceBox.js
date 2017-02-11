168e1615bc39e803ce840192307fdbdc405a708d
rodrigo-delgado <rod.delgado85@gmail.com>
2017-02-12 11:15:14 +1300

    fix buttons size

--- src/containers/activityChoiceBox.js
+++ src/containers/activityChoiceBox.js
@@ -6,11 +6,11 @@ function ActivityChoiceBox(props) {
     return (
       <div className='row'>
         <div className='columns small-centered small-10 medium-6 large-4'>
-          <h3>Learn</h3>
+          <h3 className='h3-chooce'>Learn</h3>
           <Link to="/activity/learn/sounds/1"><button className="button-radius choose">Letters</button></Link>
           <Link to="/activity/learn/words/1"><button className="button-radius choose">Words</button></Link>
 
-          <h3>Practice</h3>
+          <h3 className='h3-chooce'>Practice</h3>
           <Link to="/activity/practice/sounds/1"><button className="button-radius choose">Letters</button></Link>
           <Link to="/activity/practice/words/1"><button className="button-radius choose">Words</button></Link>
         </div>
