df9a66a420b9b4b8a36808f7cc64cece1f8d9480
jtito <jtito@hotmail.com>
2017-02-13 14:35:08 +1300

    activity page images need sorting size

--- src/containers/activityChoiceBox.js
+++ src/containers/activityChoiceBox.js
@@ -4,31 +4,36 @@ const { connect } = require('react-redux')
 
 function ActivityChoiceBox(props) {
     return (
-    <div>
-      <div className='row'>
-        <div className='columns small-centered small-10 medium-6 large-4'>
-          <h3 className='h3-choose'>Learn</h3>
-          <Link to="/activity/learn/sounds/1"><button className="choose">Letters</button></Link>
-          <Link to="/activity/learn/words/1"><button className="choose">Words</button></Link>
+      <div className='activity-div'>
 
-          <h3 className='h3-choose'>Practice</h3>
-          <Link to="/activity/practice/sounds/1"><button className="choose">Letters</button></Link>
-          <Link to="/activity/practice/words/1"><button className="choose">Words</button></Link>
+        <div className='row main'>
+          <div className='columns small-centered small-10 medium-6 large-4'>
+            <h3 className='h3-choose'>Learn</h3>
+            <Link to="/activity/learn/sounds/1"><button className="choose">Letters</button></Link>
+            <Link to="/activity/learn/words/1"><button className="choose">Words</button></Link>
+
+            <h3 className='h3-choose'>Practice</h3>
+            <Link to="/activity/practice/sounds/1"><button className="choose">Letters</button></Link>
+            <Link to="/activity/practice/words/1"><button className="choose">Words</button></Link>
+          </div>
         </div>
+
+
       </div>
-        <div className='sea'>
-          <img src='../images/decoration-images/activity/sea2.png' />
-        </div>
-        <div className='starfish'>
-          <img src='../images/decoration-images/activity/starfish.png' />
-        </div>
-        <div className='jelly-fish'>
-          <img src='../images/decoration-images/activity/jelly-fish.png' />
-        </div>
-
-    </div>
     )
 
 }
 
 module.exports = connect((state) => state)(ActivityChoiceBox)
+
+          // <div className='sea'>
+          //   <img src='../images/decoration-images/activity/sea2.png' />
+          // </div>
+          //
+          // <div className='starfish'>
+          //   <img src='../images/decoration-images/activity/starfish.png' />
+          // </div>
+          //
+          // <div className='jelly-fish'>
+          //   <img src='../images/decoration-images/activity/jelly-fish.png' />
+          // </div>
