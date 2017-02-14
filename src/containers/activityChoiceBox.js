08ceda6bd78605f0cda176952c9ae8f1d9d0c0a8
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-02-15 09:22:06 +1300

    Add new wording

--- src/containers/activityChoiceBox.js
+++ src/containers/activityChoiceBox.js
@@ -9,13 +9,13 @@ function ActivityChoiceBox(props) {
 
         <div className='row main'>
           <div className='columns small-centered small-10 medium-6 large-4'>
-            <h3 className='h3-choose'>Learn</h3>
-            <Link to="/activity/learn/sounds/1"><button className="choose">Letters</button></Link>
-            <Link to="/activity/learn/words/1"><button className="choose">Words</button></Link>
+            <h3 className='h3-choose'>Learn to pronounce Te Reo</h3>
+            <Link to="/activity/learn/sounds/1"><button className="choose">Letters and sounds</button></Link>
+            <Link to="/activity/learn/words/1"><button className="choose">Full words</button></Link>
 
-            <h3 className='h3-choose'>Practice</h3>
-            <Link to="/activity/practice/sounds/1"><button className="choose">Letters</button></Link>
-            <Link to="/activity/practice/words/1"><button className="choose">Words</button></Link>
+            <h3 className='h3-choose'>Practice your learning</h3>
+            <Link to="/activity/practice/sounds/1"><button className="choose">Letters and sounds</button></Link>
+            <Link to="/activity/practice/words/1"><button className="choose">Full words</button></Link>
           </div>
 
 
