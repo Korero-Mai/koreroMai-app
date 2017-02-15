740be73df293054a2894e33cf9f5ec7462605bf2
anna-marie-vos <amvos@outlook.com>
2017-02-15 13:07:17 +1300

    almost there

--- src/components/addplayer.js
+++ src/components/addplayer.js
@@ -8,7 +8,10 @@ class AddPlayer extends React.Component {
   // let tokenInput = null
 
   handleSave(e){
-    const{id, dispatch} = this.props
+    const{id, dispatch,groups} = this.props
+    console.log('addPlayer props',this.props);
+    const groupName = this.refs.group_name.value
+
     request.post('api/v1/players/addPlayer')
     .send({
       player_name : this.refs.player_name.value,
@@ -18,7 +21,9 @@ class AddPlayer extends React.Component {
      })
      .end((err, res)=>{
        console.log('res.body', res.body);
+       console.log('groups[groupName]', groups[groupName]);
        if (err) return console.log('error!')
+        dispatch({type:"UPDATE_PLAYERS", payload:{players: groups[groupName], group_name:groupName}})
         dispatch({type: 'UPDATE_GROUPS', payload: res.body.groups})
       })
 
@@ -26,7 +31,7 @@ class AddPlayer extends React.Component {
 
 
   render() {
-  const {alone, id} = this.props
+  const {alone, id, groups} = this.props
       return (
 
           <tr>
